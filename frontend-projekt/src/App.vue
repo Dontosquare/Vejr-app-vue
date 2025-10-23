8ec8dc24247b51ecbafb26d6870252af3acf3b3e
Dontosquare <Jokke.porsknudsen@gmail.com>
2025-10-23 17:12:46 +0200

    input felt tilføjet

--- frontend-projekt/src/App.vue
+++ frontend-projekt/src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import Header from './components/Header.vue'
+import Input from './components/Input.vue'
 import { ref } from 'vue';
 
 const result = ref(null)
@@ -15,6 +16,7 @@ fetch('73d60521ff71fd2cf36e01d86667d70d')
 <template>
   <Header></Header>
   <hr />
+  <Input></Input>
 </template>
 
 <style scoped>
---
+++ frontend-projekt/src/components/Input.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { ref } from 'vue';
+
+const message = ref('Søg efter by');
+</script>
+
+<template>
+    <div>
+        <input v-model="message" />
+    </div>
+
+</template>
