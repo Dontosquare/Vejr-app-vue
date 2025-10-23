76a5fe550c567c8bc35e4e979d311ebfbc9aa12a
Dontosquare <Jokke.porsknudsen@gmail.com>
2025-10-23 16:51:41 +0200

    checkpoint

--- frontend-projekt/src/App.vue
+++ frontend-projekt/src/App.vue
@@ -1,9 +1,20 @@
 <script setup>
+import Header from './components/Header.vue'
+import { ref } from 'vue';
 
+const result = ref(null)
+
+fetch('73d60521ff71fd2cf36e01d86667d70d')
+  .then(response => response.json())
+  .then(data => {
+    console.log(data);
+    return { result }
+  })  
 </script>
 
 <template>
-
+  <Header></Header>
+  <hr />
 </template>
 
 <style scoped>
---
+++ frontend-projekt/src/components/Header.vue
@@ -0,0 +1,9 @@
+<script setup> 
+import { ref } from 'vue';
+</script>
+
+<template>
+  <header>
+    <h1>Frontend projekt vejr app</h1>
+  </header>
+</template>
