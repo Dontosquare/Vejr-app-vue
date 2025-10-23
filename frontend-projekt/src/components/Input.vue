4177f76530f6c92b3f1822f85c432dd6fe2a38da
Dontosquare <Jokke.porsknudsen@gmail.com>
2025-10-23 17:22:14 +0200

    Sidste update

--- frontend-projekt/src/components/Input.vue
+++ frontend-projekt/src/components/Input.vue
@@ -1,12 +1,19 @@
 <script setup>
 import { ref } from 'vue';
 
-const message = ref('Søg efter by');
+const soegefterby = ref('');
+
+const onSubmitForm = () => {
+    console.log(soegefterby.value);
+    todoList.value.push(soegefterby.value);
+    soegefterby.value = '';
+    console.table(soegefterby.value);
+};
 </script>
 
 <template>
-    <div>
-        <input v-model="message" />
-    </div>
-
+    <form v-on:submit.prevent="onSubmitForm">
+        <input v-model="soegefterby" placeholder="Indtast by" />
+        <button type="submit">Søg</button>
+    </form>
 </template>
