cf5ce79aa3d3079d470a0b5e73ce7ed5484fa9ce
Dontosquare <153940558+Dontosquare@users.noreply.github.com>
2025-10-26 17:46:28 +0100

    updated

--- frontend-projekt/src/App.vue
+++ frontend-projekt/src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import Header from './components/Header.vue'
-import Input from './components/Input.vue'
+ //import Input from './components/Input.vue'
+// import Vejrtekst from './components/Vejrtekst.vue'
 import { ref } from 'vue';
 
 const result = ref(null)
@@ -25,10 +26,17 @@ const fetchWeather = async (event) => {
 <template>
   <Header></Header>
   <hr />
-  <form id="app">
+  <form>
         <input v-model="city" v-on:keypress="fetchWeather" placeholder="Indtast by" />
         <button type="submit" >Søg</button>
     </form>
+  <div v-if="result">
+      <h2>{{ result.name }}, {{ result.sys.country }}</h2>
+      <p>Temperatur: {{ result.main.temp }} °C</p>
+      <p>Vejr: {{ result.weather[0].description }}</p>
+      <p>Luftfugtighed: {{ result.main.humidity }}%</p>
+      <p>Vindhastighed: {{ result.wind.speed }} m/s</p>
+  </div>  
 </template>
 
 <style scoped>
---
+++ frontend-projekt/src/components/Vejrtekst.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+  <div v-if="result">
+      <h2>{{ result.name }}, {{ result.sys.country }}</h2>
+      <p>Temperatur: {{ result.main.temp }} °C</p>
+      <p>Vejr: {{ result.weather[0].description }}</p>
+      <p>Luftfugtighed: {{ result.main.humidity }}%</p>
+      <p>Vindhastighed: {{ result.wind.speed }} m/s</p>
+  </div>  
+</template>
+
+<style scoped>
+
+</style>
